afc98d1cc4098af5bd5b23934df1006d36ebf61a
Harsh Kumar <harshgbkumar0139@gmail.com>
2023-10-21 01:12:02 +0530

    added items using java script

---
+++ main.js
@@ -0,0 +1,63 @@
+let shop = document.getElementById("shop")
+
+
+let shopItems = [ 
+    {
+        id : "c1",
+        name: "casual shirt",
+        price : 25,
+        desc : "Lorem, ipsum dolor sit amet consectetur adipisicing",
+        img : "images/img-1.jpg"
+    } ,
+    {
+        id : "c2",
+        name: "shirt",
+        price : 85,
+        desc : "Lorem, ipsum dolor sit amet consectetur adipisicing",
+        img : "images/img-2.jpg"
+    },
+    {
+        id : "c3",
+        name: "T- shirt",
+        price : 255,
+        desc : "Lorem, ipsum dolor sit amet consectetur adipisicing",
+        img : "images/img-3.jpg"
+    },
+    {
+        id : "c4",
+        name: "Formal",
+        price : 295,
+        desc : "Lorem, ipsum dolor sit amet consectetur adipisicing",
+        img : "images/img-4.jpg"
+    }
+]
+
+
+
+
+let generateShop = ()=>{
+
+    return (shop.innerHTML = shopItems.map((x)=>{
+        return `
+        <div id = "product-id-${x.id} "  class="item">
+                <img width="220" src=" ${x.img} " alt="">
+                <div class="details">
+                    <h3>${x.name} </h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <div class="price-quantity">
+                        
+                            <h2> ${x.price} </h2>
+                        
+                        <div class="buttons">
+    
+                            <i class="bi bi-dash-lg"></i>
+                            <div id = " ${x.id} " class="quantity">0</div>
+                            <i class="bi bi-plus-lg"></i>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>`
+    } ) .join(" ") );
+}
+generateShop();
