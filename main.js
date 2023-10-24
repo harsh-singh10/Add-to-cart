b16f08f8656f95de85bbc9dc0afd58ca465ef974
Harsh Kumar <harshgbkumar0139@gmail.com>
2023-10-24 22:58:49 +0530

    added local storage in the web app

--- main.js
+++ main.js
@@ -1,123 +1,111 @@
-let shop = document.getElementById("shop")
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 
-let bascet =  [];
-
-
-let shopItems = [
-    {
-        id: "c1",
-        name: "casual shirt",
-        price: 25,
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing",
-        img: "images/img-1.jpg"
-    },
-    {
-        id: "c2",
-        name: "shirt",
-        price: 85,
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing",
-        img: "images/img-2.jpg"
-    },
-    {
-        id: "c3",
-        name: "T- shirt",
-        price: 255,
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing",
-        img: "images/img-3.jpg"
-    },
-    {
-        id: "c4",
-        name: "Formal",
-        price: 295,
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing",
-        img: "images/img-4.jpg"
-    }
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
 ];
 
-
-
-
 let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
+              </div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join(""));
+};
 
-    return (shop.innerHTML = shopItems.map((x) => {
-        let {id , name, price,desc } = x
-        let search = bascet.find((x) => x.id === id)
-       
-
-        return `
-        <div id = "product-id-${x.id} "  class="item">
-                <img width="220" src=" ${x.img} " alt="">
-                <div class="details">
-                    <h3>${x.name} </h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-                    <div class="price-quantity">
-                        
-                            <h2> $ ${x.price} </h2>
-                        
-                        <div class="buttons">
-    
-                            <i onclick = "decrement(${x.id})" class="bi bi-dash-lg"></i>
-                            <div id = "${x.id}" class="quantity">0</div>
-                            <i onclick = "increment(${x.id})" class="bi bi-plus-lg"></i>
-    
-                        </div>
-                    </div>
-                </div>
-            </div>`
-    }).join(""));
-}
 generateShop();
 
 let increment = (id) => {
-    let selectedItem = id
+  console.log(id);
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
 
-    let search = bascet.find((x) => x.id === selectedItem.id)
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
 
-    if (search === undefined) {
-        bascet.push({
-            id: selectedItem.id,
-            item: 1
-        })
-    }
-    else {
-        search.item += 1;
-    }
-
-
-   
-    // console.log(bascet);
-
-    update(selectedItem.id);
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 let decrement = (id) => {
-    let selectedItem = id
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
 
-    let search = bascet.find((x) => x.id === selectedItem.id)
-
-    if (search.item === 0) {
-        return
-    }
-    else {
-        search.item -= 1;
-    }
-
-    //console.log(bascet);
-
-    update(selectedItem.id);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 let update = (id) => {
-
-    let search = bascet.find((x) => x.id === id)
-    document.getElementById(id).innerHTML = search.item
-    //console.log(search.item)
-    calculate();
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
 };
 
-let calculate = () => {
-
-    let cartIcon = document.getElementById("cartAmount");
-      cartIcon.innerHTML = bascet.map((x) => x.item).reduce((x, y) => x + y, 0)
-
-
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
+
+calculation();
