cb27f298fc5f1c7806d8bd761af9ce2ff4942d44
Harsh Kumar <harshgbkumar0139@gmail.com>
2023-10-26 23:50:20 +0530

    made some changes in cart page

--- src/cart.js
+++ src/cart.js
@@ -11,3 +11,23 @@ let calculate = () => {
 calculate();
 
 
+
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
+
+let generateCart = ()=>{
+    if(bascet.length !== 0){
+       return (shoppingCart.innerHTML = bascet.map( (x)=>{
+        return `     <div class="cart-items">Hello</div>  `
+       } ).join("") ) 
+    }
+    else{
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+        <h2> Your cart is Empty </h2>
+        <a href = "index.html" > 
+        <button class="home"> Go to Home</button> </a> `;
+
+    }
+}
+generateCart()
